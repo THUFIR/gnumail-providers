c7179ece2ea7a308092020108fbb3ab24222fa70
Thufir Hawat <hawat.thufir@gmail.com>
2013-03-21 02:15:55 -0700

    incremental

--- src/net/bounceme/dur/nntp/gnu/Main.java
+++ src/net/bounceme/dur/nntp/gnu/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
     private final static Logger LOG = Logger.getLogger(Main.class.getName());
 
-    public static void main(String... args) {
+    Usenet u = Usenet.INSTANCE;
+
+    public Main() throws Exception {
+                LOG.info("in constructor");
+                u.connect();
+    }
+
+    public static void main(String... args) throws Exception {
         LOG.info("well here we are");
+        new Main();
     }
 }
