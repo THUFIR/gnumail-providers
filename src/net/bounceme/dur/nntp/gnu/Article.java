3665ed2a9b7b366045066597d341301c439bc79c
Thufir Hawat <hawat.thufir@gmail.com>
2013-03-21 02:19:47 -0700

    incremental

---
+++ src/net/bounceme/dur/nntp/gnu/Article.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.dur.nntp.gnu;
+
+/**
+ *
+ * @author thufir
+ */
+class Article {
+
+    String getMessageId() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ src/net/bounceme/dur/nntp/gnu/Newsgroup.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.bounceme.dur.nntp.gnu;
+
+/**
+ *
+ * @author thufir
+ */
+class Newsgroup {
+
+    Newsgroup(Page page) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    String getNewsgroup() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
