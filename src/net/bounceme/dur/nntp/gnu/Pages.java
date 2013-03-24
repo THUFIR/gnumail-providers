9a2a43b7c67542b3f91db1cd08a798a0ad95429d
Thufir Hawat <hawat.thufir@gmail.com>
2013-03-24 03:11:03 -0700

    runs fine, prints all subjects.  Need to delve into GNU code and compare article numbers to message numbers, etc, then output data.

--- src/net/bounceme/dur/nntp/gnu/Pages.java
+++ src/net/bounceme/dur/nntp/gnu/Pages.java
@@ -3,6 +3,8 @@ package net.bounceme.dur.nntp.gnu;
 import gnu.mail.providers.nntp.GMD;
 import java.util.logging.Logger;
 
+//looks ok
+
 public class Pages {
 
     private final static Logger LOG = Logger.getLogger(Pages.class.getName());
